d113c2e7c0e16fb56ef670dd86299475a6081080
budchirp <budchirp@gmail.com>
2023-07-21 19:26:48 +0300

    chore: animation improvements

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,6 +10,7 @@ import { Dialog, Transition } from '@headlessui/react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import { Menu, X } from 'lucide-react'
+import { Box } from './Box'
 
 type LinkProps = {
   label: string
@@ -48,7 +49,7 @@ const Header: React.FC = () => {
     <>
       <header
         className={cn(
-          'bg-primary border-primary fixed top-0 z-[125] flex h-18 w-full items-center transition duration-300 justify-center border-b backdrop-blur-sm bg-blend-overlay',
+          'bg-primary border-primary fixed top-0 z-[125] flex h-18 w-full items-center transition duration-300 justify-center border-b backdrop-blur bg-blend-overlay',
           isMobileMenuOpened ? 'bg-opacity-100 ease-out' : '!bg-opacity-25 ease-in'
         )}
       >
@@ -103,7 +104,7 @@ const Header: React.FC = () => {
                 setIsMobileMenuOpened(false)
               }}
               aria-hidden="true"
-              className="fixed bg-blend-overlay inset-0 z-[75] h-screen w-screen !bg-opacity-5 backdrop-blur-sm bg-primary"
+              className="md:hidden fixed bg-blend-overlay inset-0 z-[75] h-screen w-screen !bg-opacity-5 backdrop-blur bg-primary"
             />
           </Transition.Child>
 
@@ -112,32 +113,39 @@ const Header: React.FC = () => {
             enter="transition ease-out duration-150"
             enterFrom="opacity-0 scale-90"
             enterTo="opacity-100 scale-100"
-            leave="transition ease-in duration-300"
+            leave="transition ease-in duration-150"
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-90"
           >
-            <Dialog.Panel className="bg-primary border-primary fixed inset-x-0 top-[5.5rem] z-[100] mx-auto w-11/12 max-w-sm overflow-hidden rounded-2xl border md:hidden">
-              <div className="flex items-center border-b border-primary px-6 h-14">
-                <Dialog.Title as="h2" className="text-2xl h-full flex items-center leading-none font-bold">
-                  Links
-                </Dialog.Title>
-              </div>
+            <Dialog.Panel className="fixed z-[100] w-screen origin-[90%_0%] top-[5.5rem] flex justify-center inset-x-0">
+              <Container>
+                <div className="relative flex justify-right items-right w-full">
+                  <Box padding="none" variant="primary" className="overflow-hidden w-full max-w-screen-sm">
+                    <div className="flex items-center border-b border-primary px-6 h-16">
+                      <Dialog.Title as="h2" className="text-2xl h-full flex items-center leading-none font-bold">
+                        Links
+                      </Dialog.Title>
+                    </div>
 
-              <div className="px-6 my-2">
-                {links.map((link: LinkProps, index: number) => (
-                  <div key={index}>
-                    <Link
-                      className={cn(
-                        'hover:text-primary leading-none mb-1 last:mb-0 text-lg font-medium transition duration-150',
-                        link.url == pathname ? 'text-primary' : 'text-tertiary'
+                    <div className="px-6 my-2 flex flex-col">
+                      {links.map(
+                        (link: LinkProps, index: number): JSX.Element => (
+                          <Link
+                            key={index}
+                            className={cn(
+                              'hover:text-primary text-lg leading-6 mb-1 last:mb-0 font-medium transition duration-300',
+                              link.url == pathname ? 'text-primary' : 'text-tertiary'
+                            )}
+                            href={link.url}
+                          >
+                            {link.label}
+                          </Link>
+                        )
                       )}
-                      href={link.url}
-                    >
-                      {link.label}
-                    </Link>
-                  </div>
-                ))}
-              </div>
+                    </div>
+                  </Box>
+                </div>
+              </Container>
             </Dialog.Panel>
           </Transition.Child>
         </Dialog>
